a712b5def3fa7aa84961b6f073693d6aa58b617d
jonathanko500 <jkko9010@gmail.com>
2020-08-22 19:29:03 -0700

    end of lesson 1

---
+++ Intro/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //intro to printing stuff
+    cout << "Hello world!" << endl;
+    cout << "Gabe sucks" << endl << "Hunters are better." << endl;
+    cout << "I said \" But you are not as bad as those who cant counter the buisness. \"" << endl;
+    //intro to variables
+    int hoursPlayed;
+    cout << "Enter number of hours you played Destiny 2: ";
+    cin >> hoursPlayed;
+    cout << hoursPlayed;
+
+}
