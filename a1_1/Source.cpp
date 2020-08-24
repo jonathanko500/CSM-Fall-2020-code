1667fe224b8f708d7c92f7cf02d7fbd3406a9baf
jonathanko500 <jkko9010@gmail.com>
2020-08-23 20:50:38 -0700

    assignment 1
    finish asignment 1

--- a1_1/Source.cpp
+++ a1_1/Source.cpp
@@ -12,6 +12,19 @@ int main()
 	//ask user month number
 	cout << "Enter the date's month number: ";
 	cin >> monthNumb;
+	//ask user year in a 2-digit number
+	cout << "Enter the date's year number in a 2-digit format: ";
+	cin >> yearNumb;
+	//print date
+	cout << " The date you wrote is ";
+	cout << dayNumb <<"/" << monthNumb << "/" << yearNumb;
 
-	cout << dayNumb << monthNumb;
+	//output
+	/*
+	* Enter the date day's number: 2
+		Enter the date's month number: 1
+		Enter the date's year number in a 2-digit format: 88
+		The date you wrote is 2 / 1 / 88
+	*/
+	
 }
