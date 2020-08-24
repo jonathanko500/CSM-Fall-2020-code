7e3c4fb4a1d04f58ba7e3fbae5d82910fca1333f
jonathanko500 <jkko9010@gmail.com>
2020-08-23 20:41:29 -0700

    assignment 1
    begging of assignment 1

---
+++ a1_1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	//variables
+	int dayNumb, monthNumb, yearNumb;
+	//ask user for date number
+	cout << "Enter the date day's number: "; 
+	cin >> dayNumb;
+	//ask user month number
+	cout << "Enter the date's month number: ";
+	cin >> monthNumb;
+
+	cout << dayNumb << monthNumb;
+}
