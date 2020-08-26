74e01ba1c253fcba1aee4227a1affce23c897093
jonathanko500 <jkko9010@gmail.com>
2020-08-26 10:58:58 -0700

    Lesson 5

---
+++ loops2/Lesson5/loops2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+const int endDay = 15;
+
+int main()
+{
+	int numbDay;
+	for (numbDay = 0; numbDay <= endDay; numbDay++)
+	{
+		cout << "Blaze it." << endl;
+	}
+}
