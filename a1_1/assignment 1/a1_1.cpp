d56d6727b4697c69e6a17798dbcd620de7b4efba
jonathanko500 <jkko9010@gmail.com>
2020-08-31 16:07:46 -0700

    revision of assignment 1

--- a1_1/assignment 1/a1_1.cpp
+++ a1_1/assignment 1/a1_1.cpp
@@ -3,28 +3,27 @@ using namespace std;
 
 int main()
 {
-
-	//variables
-	int dayNumb, monthNumb, yearNumb;
-	//ask user for date number
-	cout << "Enter the date day's number: ";
-	cin >> dayNumb;
-	//ask user month number
-	cout << "Enter the date's month number: ";
-	cin >> monthNumb;
-	//ask user year in a 2-digit number
-	cout << "Enter the date's year number in a 2-digit format: ";
-	cin >> yearNumb;
-	//print date
-	cout << " The date you wrote is ";
-	cout << dayNumb << "/" << monthNumb << "/" << yearNumb;
-
-	//output
-	/*
-	* Enter the date day's number: 2
-		Enter the date's month number: 1
-		Enter the date's year number in a 2-digit format: 88
-		The date you wrote is 2 / 1 / 88
-	*/
-
+	int date, month, year,yearCheck;
+	char sym1, sym2;
+	cout << "Enter a date in the format of month/day/2-digit year: ";
+	cin >> month >> sym1 >> date >> sym2 >> year;
+	if (month * date == year)
+	{
+		cout << "this year is magic";
+	}
+	else
+	{
+		cout << "this year is not magic";
+	}
 }
+/*
+* 
+* Enter a date in the format of month/day/2-digit year: 6/10/60
+* this year is magic
+* 
+* Enter a date in the format of month/day/2-digit year: 6/10/61
+* this year is not magic
+* 
+* 
+* 
+*/
