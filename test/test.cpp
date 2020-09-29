7b9d1f833b608a4717e86d5b4d563af36cf685e1
jonathanko500 <jkko9010@gmail.com>
2020-09-28 19:46:47 -0700

    test
    compiled midterm ques

---
+++ test/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{//start
+	int number[] = { 99,87, 66, 55, 101 };
+	for (int i = 1; i < 4; i++)
+	{
+		cout << number[i] << endl;
+	}
+}//end
