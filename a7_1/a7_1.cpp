edb8a8b1c1113d735c800940e69df0ca804f182f
jonathanko500 <jkko9010@gmail.com>
2020-10-04 20:56:22 -0700

    assignment 7
    fin

--- a7_1/a7_1.cpp
+++ a7_1/a7_1.cpp
@@ -12,16 +12,65 @@ int numLetters(const char* inString);
 
 int main()
 {//start main
-	char str1[] = "Hello there";
-	cout << numLetters(str1) << endl;
-	cout << lastIndexOf(str1, 'e') << endl;
-	toupper(str1);
-	reverse(str1);	
-	for (int i = 0; i < numLetters(str1); i++)
+	char str1[] = "I hate writing essays";
+	cout << "str1 is: ";
+	for (int i = 0; i < strlen(str1); i++)
 	{
 		cout << str1[i];
 	}
-	
+	cout << endl;
+	//numLetter tester
+	cout << "There are " << numLetters(str1) << " letters in str1. " << endl;
+	//lastIndexOF tester
+	if (lastIndexOf(str1, 's') == 1)
+	{
+		cout << "the last letter is what you are looking for in str1" << endl;
+	}
+	else
+	{
+		cout << "no target in str1" << endl;
+	}
+	//replace tester
+	cout << "The number of letters replaced are/is " << replace(str1, 's', 'z') << " in str1" << endl;
+	cout << "str1 with replaced letter(s) look like:  ";
+	for (int i = 0; i < strlen(str1); i++)
+	{
+		cout << str1[i];
+	}
+	cout << endl;
+	//toupper tester
+	cout << "str1 in all caps look like:  ";
+	toupper(str1);
+	for (int i = 0; i < strlen(str1); i++)
+	{
+		cout << str1[i];
+	}
+	cout << endl;
+	//reverse tester
+	cout << "str1 backwards and in caps look like:  ";
+	reverse(str1);
+	for (int i = 0; i < strlen(str1); i++)
+	{
+		cout << str1[i];
+	}
+	cout << endl;
+	cout << "************************" << endl;
+	//palindrome tester
+	char str2[] = "AbBa";
+	cout << "str2 is ";
+	for (int i = 0; i < strlen(str2); i++)
+	{
+		cout << str2[i];
+	}
+	cout << endl;
+	if (isPalindrome(str2) == true)
+	{
+		cout << "str2 is a palindrome";
+	}
+	else
+	{
+		cout << "str2 is not a palindrome";
+	}
 }//end main
 
 
@@ -39,7 +88,10 @@ int numLetters(const char* inString)
 	int size = strlen(inString);
 	for (int i = 0; i < size; i++)
 	{
-		count++;
+		if (inString[i] != ' ')
+		{
+			count++;
+		}
 	}
 	return count;
 }//end
@@ -54,8 +106,8 @@ and it returns 1 or -1 if the target char is in the string
 */
 int lastIndexOf(const char* inString, char target)
 {//start
-	if (inString[numLetters(inString) - 1] == target)
-	{
+	if (inString[strlen(inString) - 1] == target)
+	{//check last index of string
 		return 1;
 	}
 	else
@@ -74,12 +126,11 @@ this functions make the string turn into upper case letters
 void toupper(char* inString)
 {//start
 	char letter;
-	for (int i = 0; i < numLetters(inString); i++)
-	{
-		
-		putchar(toupper(inString[i]));
-	}
-	cout << endl;
+	for (int i = 0; i < strlen(inString); i++)
+	{//start looking through string
+		letter = toupper(inString[i]);
+		inString[i] = letter;
+	}//end
 }//end
 
 
@@ -92,10 +143,79 @@ reverse displays the string in reverse order
 */
 void reverse(char* inString)
 {//start
-	int end = numLetters(inString);
+	int end = strlen(inString);
 	for (int i = 0; i < end; i++)
-	{
+	{//start searching through string
 		swap(inString[i], inString[end-1]);
 		end--;
+	}//end search
+}//end
+
+
+
+
+
+/*
+replace does
+*/
+int replace(char* inString, char target, char replacementChar)
+{//start
+	int count = 0;
+	for (int i = 0; i < strlen(inString); i++)
+	{//start comparing
+		if (inString[i] == target)
+		{//start replace letter and inc counter
+			count++;
+			inString[i] = replacementChar;
+		}//end replace
+	}//end search
+	//return numb of replacements
+	if (count != 0)
+	{
+		return count;
+	}
+	else
+	{
+		return 0;
 	}
 }//end
+
+
+
+
+
+/*
+isPalindrome determines the string if it is a palindrome
+not case sensitive
+*/
+bool isPalindrome(const char* inString)
+{//start
+	int start = 0;//first letter
+	int end = strlen(inString)-1;//last letter
+	while (start < end)
+	{//start look through string
+		if (tolower(inString[start]) != tolower(inString[end]))
+		{
+			return false;
+		}
+
+		start++;
+		end--;
+	}//end look
+	return true;
+}//end
+
+
+
+/*output
+str1 is: I hate writing essays
+There are 18 letters in str1.
+the last letter is what you are looking for in str1
+The number of letters replaced are/is 3 in str1
+str1 with replaced letter(s) look like:  I hate writing ezzayz
+str1 in all caps look like:  I HATE WRITING EZZAYZ
+str1 backwards and in caps look like:  ZYAZZE GNITIRW ETAH I
+************************
+str2 is AbBa
+str2 is a palindrome
+*/
