eadfb8facc03000fe3d137de0f7d2665b520e5ee
jonathanko500 <jkko9010@gmail.com>
2020-11-08 18:53:58 -0800

    assignment 12
    start

---
+++ a12_1/Fraction.h
@@ -0,0 +1,47 @@
+//header
+#include <iostream>
+#ifndef FRACTION_H
+#define FRACTION_H
+
+
+
+namespace cs_fraction
+{//start namespace
+	class Fraction
+	{//start class
+		//initialize private variable + function
+		private:
+			int top;//numerator
+			int bottom;//denominator
+			void simplify();//simplify fractions
+		//initialize overloaded operator + function + constructor(s)
+		public:
+			//constructors
+			Fraction();
+			Fraction(int x = 0, int y = 1);
+			//<<
+			ostream& operator<<(ostream& sign);
+			void print();
+			//operators
+			Fraction operator+ (const Fraction x) const;
+			Fraction operator- (const Fraction x) const;
+			Fraction operator* (const Fraction x) const;
+			Fraction operator/ (const Fraction x) const;
+			//inequality
+			bool operator< (const Fraction x) const;
+			bool operator<= (const Fraction x) const;
+			bool operator> (const Fraction x) const;
+			bool operator>= (const Fraction x) const;
+			bool operator== (const Fraction x) const;
+			bool operator!= (const Fraction x) const;
+			//increment + decrement
+			Fraction operator++();
+			Fraction operator++(int);
+			Fraction operator--();
+			Fraction operator--(int);
+
+
+
+	};//end class
+}//end namespace
+#endif
---
+++ a12_1/a12_1.cpp
@@ -0,0 +1,20 @@
+//client
+/*
+Jonathan Ko
+10/26/2020
+CIS 278
+Assignment #10
+
+This program is called Fraction
+With the help of sepperate file classes, the compiler will be able to do basic arithmetic
+computations with objects being reated as fractions.
+
+These fractions are able to be
+added
+subtracted
+multiplied
+divided
+simplified
+
+In addition to having the fractions be made, the fractions are able to be worked on by operator instead of using functions
+*/
